500231f36bb048a4e19b4c644c78874c34d3675c
tatianagutierrez <tatiana14gutierrez@gmail.com>
2023-09-27 11:37:17 -0300

    Agrego obtener juego por campo

--- controllers/juegos.js
+++ controllers/juegos.js
@@ -3,12 +3,14 @@ const axios = require('axios');
 
 const CLIENT_ID = process.env.CLIENT_ID;
 const TOKEN = process.env.TOKEN;
+const URL_BASE_JUEGOS = 'https://api.igdb.com/v4/games';
 
-const fields = "age_ratings,themes,cover,first_release_date,genres,name,platforms,summary,total_rating";
+const fields = "cover.url,first_release_date,genres.name,name,platforms.name,summary,total_rating,franchise.name";
 
-const get50Juegos = (req = request, res = response) => {
 
-    axios.get(`https://api.igdb.com/v4/games?fields=${fields}&limit=50`, {
+const get50Juegos = (_, res = response) => {
+
+    axios.get(`${URL_BASE_JUEGOS}?fields=${fields}&limit=50`, {
         headers: {
             'Client-ID': CLIENT_ID,
             'Authorization': `Bearer ${TOKEN}`
@@ -22,17 +24,18 @@ const get50Juegos = (req = request, res = response) => {
         });
     })
     .catch((error) => {
-        res.status(error.response.status).json({
+        res.status(error.response.status).json({ 
             status: error.response.status,
             msg: 'Error inesperado'
         })
     })
 }
 
+
 const getJuegoById = (req = request, res = response) => {
     const {id} = req.params; 
 
-    axios.get(`https://api.igdb.com/v4/games/${id}?fields=${fields}`, {
+    axios.get(`${URL_BASE_JUEGOS}/${id}?fields=${fields}`, {
         headers: {
             'Client-ID': CLIENT_ID,
             'Authorization': `Bearer ${TOKEN}`
@@ -68,7 +71,41 @@ const getJuegoById = (req = request, res = response) => {
     })
 }
 
+
+const getJuegoByField = (req = request, res = response) => {
+    const {name} = req.query;
+
+    axios.get(`${URL_BASE_JUEGOS}?search=${name}&fields=${fields}`, {
+        headers: {
+            'Client-ID': CLIENT_ID,
+            'Authorization': `Bearer ${TOKEN}`
+        }
+    })
+    .then(({ status, data, statusText }) => {
+        if (data.length === 0) {
+            res.status(404).json({
+                status: 404,
+                msg: 'Ningún juego coincide con el parámetro'
+            })
+        } else {
+            res.status(200).json({
+                status,
+                statusText,
+                data
+            })
+        }
+    })
+    .catch((error) => {
+        res.status(error.response.status).json({
+            status: error.response.status,
+            msg: 'Error inesperado'
+        })
+    })
+}
+
+
 module.exports = {
     get50Juegos,
-    getJuegoById
+    getJuegoById,
+    getJuegoByField
 };
--- routes/juegos.js
+++ routes/juegos.js
@@ -1,9 +1,10 @@
 const { Router } = require('express');
-const { get50Juegos, getJuegoById } = require('../controllers/juegos');
+const { get50Juegos, getJuegoById, getJuegoByField } = require('../controllers/juegos');
 
 const rutas_juegos = Router();
 
 rutas_juegos.get('/', get50Juegos);
-rutas_juegos.get('/:id', getJuegoById)
+rutas_juegos.get('/filtrar', getJuegoByField);
+rutas_juegos.get('/:id', getJuegoById);
 
 module.exports = rutas_juegos;
